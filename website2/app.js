45f6c9594c28f96a103ca1551fd637969b7ce9a0
ilovebilliesomuch <fabulous.milk.man1@gmail.com>
2025-09-17 23:55:29 +0100

    Delete website2 directory

--- website2/app.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector('.navbar_menu');
-const toggle = document.querySelector('#mobile-menu');
-
-toggle.addEventListener('click', () => {
-  menu.classList.toggle('active');
-});
